512ced82707f5f349557e1a6edd26013392ecbf8
Carhul <carolinahulloen@gmail.com>
2022-02-21 13:21:31 +0000

    Add requirements file and testing to app.py file

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-""" Import OS """
+# Imports
+""" Necessary Libary Imports """
+
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
+# Flask App Configuration
+
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+# Testing area
 
 @app.route("/")
 def test():
-    """ Test if works """
-    return "This is a test"
+    """ Test if I can find the tasks """
+    return "TEST. can you see me"
 
 
 if __name__ == "__main__":
